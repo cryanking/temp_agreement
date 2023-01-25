aea6d871257217d8f47b1f1471906adf03ffbd17
christopherking@moose <christopherking@wustl.edu>
2023-01-25 10:47:23 -0600

    add mean curve output, fix a time bug, add an exclusion criteria

--- hypothermia_incidence.R
+++ hypothermia_incidence.R
@@ -1,6 +1,6 @@
 
 
-# install.packages("lmeresampler") ## this package would make the bootstrap better, but doesn't seem to work right now
+# install.packages("lmeresampler") ## this package would make the bootstrap faster, but doesn't seem to work with other packages right now
 
 setwd("/research")
 library(dplyr)
@@ -18,6 +18,7 @@ set.seed(101)
 ## this happens to get all the column types that I care about correct
 main_data <- read_csv("ob_temp_working_data.csv")  
 main_data %<>% filter(Incl_Excl == "incl")
+main_data %<>% filter( Convert_GA == 0)
 
 starting_temperature <- 36.8
 hypothermia_threshold <- 36.0
@@ -102,7 +103,8 @@ my.boot.fun <- function(data, indicies=NULL) {
   
   modified_data$predictions<- ri_no_rs %>% predict(newdata=modified_data  ) 
   
-  modified_data %<>% group_by(Case_Number) %>% mutate(coldnow = predictions - first(predictions) + starting_temperature)
+  modified_data %<>% arrange(Case_Number, timepoint)
+  modified_data %<>% group_by(Case_Number) %>% mutate(coldnow = predictions - first(predictions) + starting_temperature) %>% ungroup
 ## main ananlyis: smoothed temperatures
 ## 
   ## average change
@@ -113,7 +115,7 @@ my.boot.fun <- function(data, indicies=NULL) {
   average_loss <- modified_data %>% group_by(Case_Number) %>% 
     summarize(
       delta0=max_loss(predictions), delta_600 = max_loss(predictions[timepoint <= 60] ) 
-      , time_to_min = which.min(predictions)
+      , time_to_min = timepoint[which.min(predictions)]
       ) %>% 
     summarize(
       time_to_min = mean(time_to_min) ,
@@ -125,18 +127,45 @@ my.boot.fun <- function(data, indicies=NULL) {
       hypothermia_ever_low= delta0 %>% add(starting_temperature) %>% is_less_than(hypothermia_threshold2) %>% mean, 
       hypothermia_60= delta_600 %>% add(starting_temperature) %>% is_less_than(hypothermia_threshold) %>% mean) %>% 
       unlist
-      average_loss <- c( average_loss, measured_very_low=   modified_data %>% group_by(Case_Number) %>% summarize(very_low = any(predictions < hypothermia_threshold2, na.rm=TRUE) ) %>% summarize(mean(very_low)) %>% unlist )
+  
+  average_loss <- c( average_loss, measured_very_low=   modified_data %>% group_by(Case_Number) %>% summarize(very_low = any(predictions < hypothermia_threshold2, na.rm=TRUE) ) %>% summarize(mean(very_low)) %>% unlist )
 
+  ## sensitivity analysis: only actually measured timepoints
+  exclude_unmeasured <- modified_data %>% semi_join(data %>% filter(!is.na(temper)), by=c("Case_Number", "timepoint")) %>% group_by(Case_Number) %>% 
+    summarize(
+      delta0=max_loss(predictions), delta_600 = max_loss(predictions[timepoint <= 60] ) 
+      , time_to_min = timepoint[which.min(predictions)]
+      ) %>% 
+    summarize(
+      time_to_min = mean(time_to_min) ,
+      delta_ever=mean(delta0), 
+      delta_60=mean(delta_600), 
+      less_1 = mean(delta0 < -1) , 
+      less_1_60=mean(delta_600 < -1) , 
+      hypothermia_ever= delta0 %>% add(starting_temperature) %>% is_less_than(hypothermia_threshold) %>% mean, 
+      hypothermia_ever_low= delta0 %>% add(starting_temperature) %>% is_less_than(hypothermia_threshold2) %>% mean, 
+      hypothermia_60= delta_600 %>% add(starting_temperature) %>% is_less_than(hypothermia_threshold) %>% mean) %>% 
+      unlist 
+  names( exclude_unmeasured) %<>% paste0("_ex")
+      
+  average_loss <- c(average_loss,exclude_unmeasured)
+  
+  ## curve on smoothed mean for CI
+  mean_curve <- modified_data %>%filter((timepoint %% 10  == 0)) %>% group_by(timepoint) %>% summarize(smooth_temp=mean(predictions ) ) %>% arrange(timepoint)
   
   
   if(is.null(indicies)) {
-    return(list(c(average_loss ), modified_data,ri_no_rs ) )
+    return(list(c(average_loss ), modified_data,ri_no_rs,mean_curve ) )
   } else {
   ## get bootstrap variability of average overall_trend
     fake_data <- data.frame(Case_Number=modified_data$Case_Number[1], timepoint=seq(from=0, to=max_time_in_prediction, by=1) )
     fake_data %<>% bind_cols(fake_data$timepoint %>% bs_fun %>%  as.data.frame %>% set_colnames( colnames(modified_data) %>% keep( ~grepl(.x, pattern="^ts")) )  )
     fake_data$predictions <- predict(ri_no_rs , newdata=fake_data, re.form=NA )
-    return(c(average_loss , fake_data$predictions ))
+    temp1 <- fake_data$predictions
+    temp2 <- mean_curve$smooth_temp
+    names(temp1) <- paste0("p_", fake_data$timepoint)
+    names(temp2) <- paste0("m_", mean_curve$timepoint)
+    return(c(average_loss , temp1 , temp2 ))
   }
   
 }
@@ -144,7 +173,7 @@ my.boot.fun <- function(data, indicies=NULL) {
 set.seed(101)
 re_result <- main_data %>% my.boot.fun
 
-boot_out_re <- boot(data=main_data , R=1000, statistic=my.boot.fun, sim = "ordinary" , ncpus=7L, parallel="multicore")
+boot_out_re <- boot(data=main_data , R=1000, statistic=my.boot.fun, sim = "ordinary" , ncpus=4L, parallel="multicore")
 saveRDS(boot_out_re, "re_boot.RDS")
 re_result %>% saveRDS("re_result.RDS")
 
@@ -152,15 +181,32 @@ re_result[[2]] %>% select(Case_Number, timepoint, predictions) %>% write_csv("sm
 
 ci_holder <- NULL
 
-for(index in seq(ncol(boot_out_re$t) - max_time_in_prediction - 1L)) {
+used_index <- names(boot_out_re$t0 ) %>% grep(pattern="^[mp]_", invert=T)
+
+for(index in  used_index) {
+#   ci_holder <- rbind( ci_holder , boot.ci(boot_out_re, index =index , type=c("perc") )$perc[ ,4:5] )
   ci_holder <- rbind( ci_holder , boot.ci(boot_out_re, index =index , type=c("bca") )$bca[ ,4:5] )
 }
 
 ci_holder <- cbind(re_result[[1]], ci_holder)
 colnames(ci_holder) <- c("est", "lower.ci", "upper.ci")
-rownames(ci_holder) <- names(re_result[[1]])
+rownames(ci_holder) <- names(re_result[[1]])[used_index]
 
 ci_holder %>% round(2) %>% write.csv("main_outs.csv")
+
+measured_index <- names(boot_out_re$t0 ) %>% grep(pattern="^m_")
+
+ci_measured_holder <- NULL
+for(index in  measured_index) {
+  ci_measured_holder <- rbind( ci_measured_holder , boot.ci(boot_out_re, index =index , type=c("perc") )$perc[ ,4:5] )
+#   ci_measured_holder <- rbind( ci_measured_holder , boot.ci(boot_out_re, index =index , type=c("bca") )$bca[ ,4:5] )
+}
+
+ci_measured_holder <- cbind(boot_out_re$t0[measured_index] , ci_measured_holder) %>% as.data.frame
+colnames(ci_measured_holder) <- c("est", "lower.ci", "upper.ci")
+ci_measured_holder$timepoint <- names(boot_out_re$t0 ) %>% grep(pattern="^m_", value=T) %>% sub(pattern="^m_", replacement="") %>% as.numeric
+
+
 
 # fake_data <- data.frame(Case_Number=re_result[[2]]$Case_Number[1], timepoint=seq(from=0, to=max_time_in_prediction, by=1) )
 # fake_data %<>% bind_cols(fake_data$timepoint %>% bs_fun %>%  as.data.frame %>% set_colnames( colnames(re_result[[2]]) %>% keep( ~grepl(.x, pattern="^ts")) )  )
@@ -183,17 +229,21 @@ dev.off()
 
 ## alpha lines
 
-real_data %>% group_by(timepoint) %>% summarize(smooth_temp=mean(coldnow ) ) %>% plot(type="l", lwd=4, ylim=c(35,38), col='red')
+real_data %>% group_by(timepoint) %>% summarize(smooth_temp=mean(coldnow ) ) %>% plot(type="l", lwd=4, ylim=c(35,38), col='red', xlab="time since SA", ylab="smoothed temperature")
 
 real_data  %>%arrange(Case_Number, timepoint) %>% group_by(Case_Number) %>% group_walk( ~lines(.x$timepoint, .x$coldnow, lwd=1) )
 
-mycol <- rgb(0, 0, 0 , max = 255, alpha = 100, names = "black50")
+mycol <- rgb(0, 0, 0 , maxColorValue = 255, alpha = 100, names = "black50")
+mycol2 <- rgb(255, 0, 0 , maxColorValue = 255, alpha = 80, names = "red50")
 
-jpeg("overlay_variation.jpeg")
+jpeg("overlay_variation2.jpeg")
 
-real_data %>% group_by(timepoint) %>% summarize(smooth_temp=mean(predictions ) ) %>% plot(type="l", lwd=4, ylim=c(35,38), col='red')
+real_data %>% group_by(timepoint) %>% summarize(smooth_temp=mean(predictions ) ) %>% plot(type="l", lwd=4, ylim=c(35,38), col='red', xlab="time since SA", ylab="smoothed temperature")
 
 real_data  %>%arrange(Case_Number, timepoint) %>%filter(is.finite(temper) ) %>% filter(Case_Number %in% seq(100)) %>% group_by(Case_Number) %>% group_walk( ~lines(.x$timepoint, .x$predictions, lwd=1, col=mycol) )
+
+polygon( c(ci_measured_holder$timepoint, rev(ci_measured_holder$timepoint)) , c( ci_measured_holder$lower.ci , rev(ci_measured_holder$upper.ci) ) , col=mycol2, border=NA)
+
 
 dev.off()
 
@@ -257,7 +307,7 @@ cat_vars <- c(
 )
 
 
-tab3 <- CreateTableOne(vars = c(cont_vars, cat_vars) , strata = "delta_1" , data = main_data   , factorVars = cat_vars,  includeNA = FALSE, argsExact=list(simulate.p.values=TRUE))
+tab3 <- CreateTableOne(vars = c(cont_vars, cat_vars) , strata = "delta_1" , data = main_data   , factorVars = cat_vars,  includeNA = FALSE, argsExact=list(simulate.p.values=TRUE), addOverall = TRUE)
 
 tab3 %>% print(contDigits=0, printToggle=FALSE, nonnormal=TRUE, smd=TRUE) %>% (kableExtra::kbl) %>% kableExtra::save_kable(file="table1.html")
 tab3 %>% print(contDigits=0, printToggle=FALSE, nonnormal=TRUE, smd=TRUE) %>% write.csv("table1.csv")
